86e116c04fd7075b2938a767f70daf42902b9ba3
菅家俊太 <shunta@MacBook-Pro.local>
2020-10-19 20:35:04 +0900

    fin countdowntimer ver1

---
+++ script.js
@@ -0,0 +1,33 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const titleEl = document.getElementById('title');
+
+const moneyBack = "16 Nov 2020";
+
+function countDown() {
+  const moneyBackDate = new Date(moneyBack);
+  const currentDate = new Date();
+
+  const totalSeconds =new Date(moneyBackDate - currentDate) /1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60; 
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
+
+  let titleview =`${days}d ${hours}h ${mins}m ${seconds}s`
+
+  titleEl.innerHTML = titleview;
+}
+
+setInterval(countDown, 1000);
+
+countDown();
